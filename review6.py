64708a98f32111e54611159b42f9a83f6ff6d14b
icekat44 <icekat44@gmail.com>
2025-06-13 00:34:05 -0400

    Commit Week 6 Review questions

---
+++ review6.py
@@ -0,0 +1,5 @@
+# import sys
+# input = sys/stdin.read
+items = input().split()
+
+print(items[::-1])
---
+++ review63.py
@@ -0,0 +1,5 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
